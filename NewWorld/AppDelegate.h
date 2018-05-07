c439a7456e6d77963ed3be3a52a79ea0df3b1dd9
Shubham <shubhamkatta23@gmail.com>
2018-05-07 15:48:44 +0530

    Initial Commit

---
+++ NewWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewWorld
+//
+//  Created by Shubham on 07/05/18.
+//  Copyright © 2018 Shubham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewWorld
+//
+//  Created by Shubham on 07/05/18.
+//  Copyright © 2018 Shubham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
